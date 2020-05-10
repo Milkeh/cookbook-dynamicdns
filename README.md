8bcd9832e6ba5285dcec0507b342f39480b32015
Harley McKenzie <mckenzie.harley@gmail.com>
2020-05-10 18:12:31 +0100

    Newly generated Chef cookbook

---
+++ README.md
@@ -0,0 +1,4 @@
+# cookbook-dynamicdns
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: cookbook-dynamicdns
+# Recipe:: default
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
