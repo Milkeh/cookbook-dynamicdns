309f44926a61c5a4c8f9caf8f00c296c8ccf8071
Harley McKenzie <mckenzie.harley@gmail.com>
2020-05-26 22:08:02 +0100

    Created initial recipe. Proper commit histories to come..

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# cookbook-dynamicdns
+# Route 53 Dynamic DNS
 
-TODO: Enter the cookbook description here.
+Download and configure the Route 53 Dynamic DNS script used to populate the A record for hmckenzie.net.
 
+### To-Do
+
+* Use chef attributes to automatically populate the configuration file using a template
+* Add git functionality to pull script from existing hmckenzie-scripts repo
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,4 +2,29 @@
 # Cookbook:: cookbook-dynamicdns
 # Recipe:: default
 #
-# Copyright:: 2020, The Authors, All Rights Reserved.
+# Copyright:: 2020, Harley McKenzie, All Rights Reserved.
+
+script_dir = '/opt/hmckenzie-scripts/'
+config_dir = '/etc/dynamic-dns/'
+
+package 'jq'
+
+git script_dir do
+  repository 'git://github.com/Milkeh/hmckenzie-scripts.git'
+  revision 'dev'
+  action :checkout
+end
+
+directory config_dir do
+  recursive true
+end
+
+cookbook_file '/etc/dynamic-dns/domain.config' do
+  source 'domain.config'
+end
+
+cron_d 'dynamic-dns' do
+  action :create
+  minute '*/5'
+  command '/opt/hmckenzie-scripts/dynamic-dns.sh | tee -a /var/log/dynamic-dns.log'
+end
